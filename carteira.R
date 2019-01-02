b13c869c05cdc7268c2c759301c372ecefd9683c
gabrielakoreeda <gabriela.koreeda@gmail.com>
2019-01-02 15:00:36 -0200

    Add taxes

--- carteira.R
+++ carteira.R
@@ -1,8 +1,9 @@
 library(data.table)
 
-# Data
-datas <- c(as.Date('2017-01-02'), Sys.Date())
+# Date
+dates <- c(as.Date('2017-01-02'), Sys.Date())
 
+# Rate Taxes -------------------------------------------------------------------
 # Selic 
 url_selic <- paste0('http://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados?formato=csv&dataInicial=',
                     format(datas[1], '%d/%m/%Y'), '&dataFinal=', format(datas[2], '%d/%m/%Y'))
@@ -20,6 +21,34 @@ url_CDI <- paste0('http://api.bcb.gov.br/dados/serie/bcdata.sgs.12/dados?formato
                     format(datas[1], '%d/%m/%Y'), '&dataFinal=', format(datas[2], '%d/%m/%Y'))
 CDI <- fread(url_CDI)
 CDI$data <- as.Date(CDI$data, format = '%d/%m/%Y')
+
+# Investments ------------------------------------------------------------------
+
+
+# Taxes ------------------------------------------------------------------------
+# Tabela Regressiva
+tabela_regressiva <- function(dateInit, dateFinal) {
+  dias <- as.numeric(difftime(dateFinal, dateInit, units = 'days'))
+  
+  if (dias < 360) {
+    ir <- 0.2
+  } else if (dias < 720 & dias >= 360) {
+    ir <- 0.175
+  } else if (dias >= 720) {
+    ir <- 0.15
+  }
+  
+  return(ir)
+}
+
+# IOF
+IOF <- function(dateInit, dateFinal) {
+  dias <- as.numeric(difftime(dateFinal, dateInit, units = 'days'))
+  
+  if (dias < 30){
+    return(0.0638)
+  }
+}
 
 
 
