27f6fb5772759586bbc4c9a506faa6524874f5a3
gabrielakoreeda <gabriela.koreeda@gmail.com>
2019-02-10 15:31:30 -0200

    Add graphs, change example and yield calculation

--- carteira.R
+++ carteira.R
@@ -1,8 +1,10 @@
 library(data.table)
 library(plotly)
+library(dplyr)
+library(zoo)
 
 # Data
-dates <- c(as.Date('2017-01-02'), Sys.Date())
+dates <- c(as.Date('2016-10-01'), Sys.Date())
 
 # Índices -------------------------------------------------------------------
 # Selic 
@@ -64,30 +66,86 @@ carteira <- read.csv2("carteira.csv", stringsAsFactors = F, header = TRUE,
                                      "character", "Date", "Date"),
                       dec = ".")
 
+rendimento_tempo_total <- data.frame(Data=as.Date(numeric()),
+                                           stringsAsFactors=FALSE) 
+
 for (i in 1:nrow(carteira)){
   switch(carteira$Indexado[i],
          "CDI" = {
-           rendimento <- (tail(carteira$`Aplicação`[i]*cumprod(1 + carteira$Rentabilidade[i]/100*as.numeric(CDI[data >= carteira$`Início`[i]]$valor)/100), n = 1) - carteira$`Aplicação`[i])
+           rendimento_tempo <- data.frame("Data" = CDI[data >= carteira$`Início`[i]]$data,
+             "CDI" = carteira$`Aplicação`[i]*cumprod(1 + carteira$Rentabilidade[i]/100*as.numeric(CDI[data >= carteira$`Início`[i]]$valor)/100),
+             stringsAsFactors = F)
+           rendimento <- (tail(rendimento_tempo$CDI, n = 1) - carteira$`Aplicação`[i])
            
            }, 
          "IPCA" = {
-           rendimento <- (tail(carteira$`Aplicação`[i]*cumprod(1 + (((1 + carteira$Rentabilidade[i]/100)^(1/12) - 1)*100 + as.numeric(IPCA[data >= format(as.Date(carteira$`Início`[i]), '%Y-%m-01')]$valor))/100), n = 1) 
-                      - carteira$`Aplicação`[i])
+           rendimento_tempo <- data.frame("Data" = IPCA[data >= carteira$`Início`[i]]$data,
+                                          "IPCA" = carteira$`Aplicação`[i]*cumprod(1 + (((1 + carteira$Rentabilidade[i]/100)^(1/12) - 1)*100 + as.numeric(IPCA[data >= carteira$`Início`[i]]$valor))/100),
+                                          stringsAsFactors = F)
+           rendimento <- (tail(rendimento_tempo$IPCA, n = 1) 
+                          - carteira$`Aplicação`[i])
          }, 
          "-" = {
-           rendimento <- (tail(carteira$`Aplicação`[i]*cumprod(1 + (((1 + carteira$Rentabilidade[i]/100)^(1/12) - 1)*100)*rep(1, length(seq(from=as.Date(carteira$`Início`[i]), to=Sys.Date(), by='month')) - 1)/100), n = 1)
+           rendimento_tempo <- data.frame("Data" = seq(from=as.Date(carteira$`Início`[i]), to=Sys.Date(), by='month'),
+                                          "Prefix" = carteira$`Aplicação`[i]*cumprod(1 + (((1 + carteira$Rentabilidade[i]/100)^(1/12) - 1)*100)*rep(1, length(seq(from=carteira$`Início`[i], to=Sys.Date(), by='month')))/100),
+                                          stringsAsFactors = F)
+           rendimento <- (tail(rendimento_tempo$Prefix, n = 1)
                            - carteira$`Aplicação`[i])
          },
          "Selic" = {
-           rendimento <- (tail(carteira$`Aplicação`[i]*cumprod(1 + ((1 + carteira$Rentabilidade[i]/100)^(1/360) - 1) + as.numeric(selic[data >= carteira$`Início`[i]]$valor)/100), n = 1) 
+           rendimento_tempo <- data.frame("Data" = selic[data >= carteira$`Início`[i]]$data,
+                                          "Selic" = carteira$`Aplicação`[i]*cumprod(1 + ((1 + carteira$Rentabilidade[i]/100)^(1/252) - 1) + as.numeric(selic[data >= carteira$`Início`[i]]$valor)/100),
+                                          stringsAsFactors = F)
+           rendimento <- (tail(rendimento_tempo$Selic, n = 1) 
                      - carteira$`Aplicação`[i])
          })
+  
+  rendimento_tempo_total <- merge(rendimento_tempo_total, rendimento_tempo, by = 'Data', all = T) %>% 
+    do(na.locf(.))
+  rendimento_tempo_total$Data <- as.Date(rendimento_tempo_total$Data)
   
   carteira$Rendimento[i] <- round(rendimento, digits = 2)
   carteira$Imposto[i] <- round((tabela_regressiva(carteira$`Início`[i], Sys.Date())
                      + IOF(carteira$`Início`[i], Sys.Date()))*carteira$Rendimento[i],
                      digits = 2)
-
-  carteira$Bruto[i] <- carteira$Rendimento[i]
-  carteira$`Líquido`[i] <- carteira$Rendimento[i] - carteira$Imposto[i]
+  
+  carteira$`Bruto`[i] <- carteira$Aplicação[i] + carteira$Rendimento[i]
+  carteira$`Líquido`[i] <- carteira$Aplicação[i] + carteira$Rendimento[i] - carteira$Imposto[i]
 }
+
+rendimento_tempo_total[c("Selic", "IPCA", "CDI", "Prefix")] <- as.numeric(unlist(rendimento_tempo_total[c("Selic", "IPCA", "CDI", "Prefix")]))
+
+rendimento_tempo_total <- rendimento_tempo_total %>%
+  mutate("Patrimônio" = rowSums(.[2:5], na.rm = TRUE)) %>%
+  select(Data, `Patrimônio`)
+
+# Visualizações ----------------------------------------------------------------
+# Distribuição
+d <- plot_ly(carteira, labels = ~Ativo, values = ~`Líquido`, type = 'pie', 
+             showlegend = T) %>%
+  layout(title = "Distribuição da Carteira - Patrimônio Líquido")
+
+# Maior rendimento
+r <- plot_ly(carteira, x = ~Ativo, 
+        y = ~(Bruto - `Aplicação`)*100/(`Aplicação`)/(as.numeric(difftime(Sys.Date(), `Início`, units = 'days'))),
+        type = 'bar') %>%
+  layout(title = "Rendimento Proporcional", yaxis = list(title = "Rendimento (%) / dias"))
+
+# Rendimento ao longo do tempo
+aporte <- carteira[c("Início", "Aplicação")] %>%
+  arrange(`Início`) %>%
+  mutate("Aporte" = cumsum(`Aplicação`)) %>%
+  select("Início", "Aporte")
+aporte <- rbind(aporte, data.frame("Início" = tail(rendimento_tempo_total$Data, n = 1),
+                                   "Aporte" = tail(aporte$Aporte, n = 1)))
+
+t <- plot_ly(rendimento_tempo_total, x = ~Data, y = ~`Patrimônio`, 
+             type = 'scatter', mode = 'lines', name = "Patrimônio") %>%
+  add_lines(x = aporte$`Início`, y = aporte$`Aporte`, 
+            line = list(shape = "hv"), name = "Aporte") %>%
+  layout(title = "Patrimõnio Líquido",
+         xaxis = list(title = ""),
+         yaxis = list(title = "R$"))
+
+
+
